918b3486d0debd3f8c6f3bfbda47a457bb48afe9
Stricks1 <gmalheiross@gmail.com>
2020-10-08 13:04:36 -0300

    fixed combined reducer for books

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,25 +1,23 @@
-const bookTest = {
-  books: [
-    {
-      id: 1,
-      title: 'some Book',
-      category: 'Action',
-    },
-    {
-      id: 2,
-      title: 'another Book',
-      category: 'Horror',
-    },
-  ],
-};
+const bookTest = [
+  {
+    id: 1,
+    title: 'some Book',
+    category: 'Action',
+  },
+  {
+    id: 2,
+    title: 'another Book',
+    category: 'Horror',
+  },
+];
 
 function books(state = bookTest, action) {
   switch (action.type) {
     case 'CREATE_BOOK':
-      return { books: [...state.books, action.book] };
+      return [...state, action.book];
 
     case 'REMOVE_BOOK':
-      return { books: [...state.books.filter(item => item.id !== action.book.id)] };
+      return [...state.filter(item => item.id !== action.book.id)];
     default:
       return state;
   }
